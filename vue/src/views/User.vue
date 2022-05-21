beea850e0a8236d55739a178560da5e01701e912
youFacai <1439011429@qq.com>
2022-05-21 20:02:57 +0800

    feature:按钮鉴权

--- vue/src/views/User.vue
+++ vue/src/views/User.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>普通用户模块</div>
+ <div>
+   <div v-limits="['superAdmin','admin','member']">普通用户模块</div>
+   <div v-limits="['superAdmin','admin']">当权限为管理员和超级管理员才显示</div>
+ </div>
 </template>
 
 <script>
