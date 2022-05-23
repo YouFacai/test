6dd8f1b91020124f54f6799b5450dcaace49747e
youFacai <1439011429@qq.com>
2022-05-23 17:08:17 +0800

    feature：组合式继承&寄生式继承

---
+++ js继承/组合式继承&寄生式继承.js
@@ -0,0 +1,31 @@
+function person() {
+    this.personName = "yzl"
+    this.like = [1,2,3]
+}
+
+function son() {
+    person.call(this);
+    this.sonName = "lte"
+}
+
+person.prototype.sayfather = function () {
+    console.log("爸爸")
+}
+
+/**
+ * 组合式继承使用 new Person()
+ * 寄生式继承使用 Object.create(person.prototype)
+ *
+ * 优势 ： 避免了两次调用 Person函数
+ * */
+son.prototype = Object.create(person.prototype)
+
+let son1 = new son();
+let son2 = new son();
+
+console.log(son1.like === son2.like)
+
+// 报错call继承会导致父原型上的方法丢失
+son1.sayfather()
+
+
