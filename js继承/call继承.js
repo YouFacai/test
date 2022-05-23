76ae5c0b19a090636b7afb851419cf8a71ccfa74
youFacai <1439011429@qq.com>
2022-05-23 17:05:35 +0800

    feature：call继承 弊端：虽然深度继承了父的实例属性，但是父原型上的方法会丢失

---
+++ js继承/call继承.js
@@ -0,0 +1,22 @@
+function person() {
+    this.personName = "yzl"
+    this.like = [1,2,3]
+}
+
+function son() {
+    person.call(this);
+    this.sonName = "lte"
+}
+
+let son1 = new son();
+let son2 = new son();
+
+console.log(son1.like === son2.like)
+person.prototype.sayfather = function () {
+    console.log("爸爸")
+}
+
+// 报错call继承会导致父原型上的方法丢失
+son1.sayfather()
+
+
