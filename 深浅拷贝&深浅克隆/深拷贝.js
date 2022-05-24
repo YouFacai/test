4c0cf52cea625a64eb24c6218ec561523d003366
youFacai <1439011429@qq.com>
2022-05-24 11:13:23 +0800

    feature: 引入weakMap解决递归引用引起的栈溢出

--- 深浅拷贝&深浅克隆/深拷贝.js
+++ 深浅拷贝&深浅克隆/深拷贝.js
@@ -13,12 +13,20 @@ let obj = {
 
 let arr = [1, 2, 3, 4]
 
-function deepClone(target) {
+function deepClone(target, weakMap = new WeakMap()) {
     let result = myType(target) === "Object" ? {} : [];
+    /**
+     * 判断weakMap中是否缓存过+ 缓存过就退出递归
+     * */
+    if(weakMap.has(target)){
+        return weakMap.get(target)
+    }
+
     for (const key in target) {
         let cur = target[key];
+        weakMap.set(target,result)
         if (typeof cur === "object") {
-            result[key] = deepClone(cur);
+            result[key] = deepClone(cur,weakMap);
         } else {
             result[key] = cur
         }
@@ -26,12 +34,18 @@ function deepClone(target) {
     return result
 }
 
+/*-----------------------*/
+// 递归的引用
+obj.obj1 = obj
+
 let deepObj = deepClone(obj)
 // 检测对象深拷贝了没有
 deepObj.like.one = "变了"
 // 检测数组深拷贝了没有
 deepObj.arr.push(5)
 console.log(obj, deepObj)
+
+
 
 function myType(target) {
     return Object.prototype.toString.call(target).match(/\s(.*)\]/)[1]
