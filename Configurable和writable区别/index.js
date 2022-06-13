16b6cae63f01dc63c01e24b0ca6ae3b78c868a20
youFacai <1439011429@qq.com>
2022-06-13 21:16:28 +0800

    feature:Configurable和writable区别

---
+++ Configurable和writable区别/index.js
@@ -0,0 +1,20 @@
+global.obj = {
+    name: "yzl",
+    age: 123,
+    like: [1, 2, 3]
+}
+
+Object.defineProperty(global,'obj',{
+    /**
+     * 只读
+     * 支队当前劫持的对象有效
+     * */
+    writable:false,
+    // 如果是只读,那么有set属性就是不对的
+    // set(val){
+    //     return val
+    // }
+})
+
+obj = '李雨澍'
+console.log(obj)
