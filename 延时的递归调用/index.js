f44b7374339f56e777f30dd38b431e5f9a2b0430
youFacai <1439011429@qq.com>
2022-06-13 21:16:45 +0800

    feature:延时的递归调用

---
+++ 延时的递归调用/index.js
@@ -0,0 +1,51 @@
+function PlayBoy(name) {
+    this.name = name
+    this.stack = [];
+    setTimeout((params) => {
+        this.next()
+    },0)
+}
+
+const boy = new PlayBoy('Tom')
+// 输出
+// 大家好我是Tom
+// 1s 之后
+// 我在玩王者
+// 2s 之后
+// 我在玩跳一跳
+
+
+PlayBoy.prototype.sayHi = function () {
+    const fn = (params) => {
+        console.log(`大家好我是${this.name}`)
+        this.next();
+    }
+    this.stack.push(fn)
+    return this
+}
+
+PlayBoy.prototype.sleep = function (time) {
+    const fn = (params) => {
+        setTimeout((params) => {
+            this.next();
+        }, time)
+    }
+    this.stack.push(fn)
+    return this
+}
+
+PlayBoy.prototype.play = function (title) {
+    const fn = (params) => {
+        console.log(`我在${title}`)
+        this.next()
+    }
+    this.stack.push(fn)
+    return this
+}
+
+PlayBoy.prototype.next = function () {
+    let fn = this.stack.shift()
+    fn && fn();
+}
+
+boy.sayHi().sleep(1000).play('王者').sleep(2000).play('跳一跳')
