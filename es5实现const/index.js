eaa44762e1b6bda07df810ab06ece755637eaeef
youFacai <1439011429@qq.com>
2022-09-07 16:41:34 +0800

    feature:es5实现const

--- es5实现const/index.js
+++ es5实现const/index.js
@@ -5,6 +5,7 @@ function _const(key,val) {
     if(!val) throw Error("const必须赋初始值")
     global[key] = val;
     Object.defineProperty(global,key,{
+        // 因为const定义的变量不会再window全局下出现，所以把这个属性配置为不可叠代让他在全局window中遍历不到
         enumerable:false,
         configurable:true,
         get(){
