aaf3b7d5594530566d9ad6093016c4920b053cfa
youFacai <1439011429@qq.com>
2022-09-14 13:13:02 +0800

    feature:统计一个html有多个种标签

--- 检测一个函数是否是new调用的/使用instanceof.js
+++ 检测一个函数是否是new调用的/使用instanceof.js
@@ -1,10 +1,19 @@
-function f() {
-    if(this instanceof f){
-        console.log("ok")
-    }else{
-        throw new Error("我错了")
+var minEatingSpeed = function(piles, h) {
+    piles.sort((a,b)=>a-b)
+    if(piles.length == h) return piles[piles.length-1];
+    let index= piles[0];
+    let count = 0;
+    while(count != h){
+        count = 0;
+        for(let val of piles){
+            count+=Math.ceil(val / index)
+        }
+        if(count > h) {
+            index++;
+            continue;
+        };
+        if(count <= h) return index;
     }
-}
+};
 
-// f()
-new f()
+console.log(minEatingSpeed([312884470],312884469))
