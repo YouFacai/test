080599254ce716e8b666ed23e71b57fa998b2398
youFacai <1439011429@qq.com>
2022-09-17 20:33:08 +0800

    feature:数组扁平化三种方法

--- 数组扁平化/index.js
+++ 数组扁平化/index.js
@@ -1,13 +1,32 @@
 let arr = [1,[2,3,[4,5]]];
-function _flat(arr,temp) {
-    for(val of arr){
-        if(Array.isArray(val)){
-            _flat(val,temp)
-        }else{
-            temp.push(val)
-        }
-    }
-    return temp
+
+// 方法一
+// function _flat(arr,temp) {
+//     for(val of arr){
+//         if(Array.isArray(val)){
+//             _flat(val,temp)
+//         }else{
+//             temp.push(val)
+//         }
+//     }
+//     return temp
+// }
+//
+// console.log(_flat(arr,[]))
+
+// 方法二
+// function _flat(arr) {
+//     while(arr.some(item=>Array.isArray(item))){
+//         arr = [].concat(...arr)
+//     }
+//     return arr
+// }
+//
+// console.log(_flat(arr))
+
+// 方法三
+function _flat(arr) {
+    return arr.toString().split(",")
 }
 
-console.log(_flat(arr,[]))
+console.log(_flat(arr))
