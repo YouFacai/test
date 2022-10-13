34dc098c9d4ae8427e72efbe5c461be13e366e27
youFacai <1439011429@qq.com>
2022-10-13 13:40:36 +0800

    feature:检测一个对象是否循环引用.js

---
+++ 算法/检测一个对象是否循环引用.js
@@ -0,0 +1,22 @@
+function _fn(target){
+    function deep(target,weakMap = new WeakMap()) {
+        weakMap.set(target,target)
+
+        for(let key in target){
+            let cur = target[key]
+            if(typeof cur == 'object'){
+                if(weakMap.has(target) || deep(cur,weakMap)){
+                    return true
+                }
+            }
+        }
+        return  false
+    }
+
+    return typeof  target == 'object' ? deep(target):false
+}
+let obj = {
+
+}
+obj.a = obj
+console.log(_fn(obj))
